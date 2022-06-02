b40cb88cff476a670a76de27607bcc9ee1527269
PandharkarSayali <97429105+PandharkarSayali@users.noreply.github.com>
2022-06-02 12:24:24 +0530

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -20,7 +20,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     console.log("abc");
     console.log("123");
-     console.log("abc");;
-    
+     console.log("abc");
+        console.log("123");
   }
 }
